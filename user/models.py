9c4c93761d8b427dc33990aa55addac13032ba2c
HosseinAsadi <HosseinA.9876@gmail.com>
2021-07-23 15:04:43 +0430

    added user profile model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 
-# Create your models here.
+
+class UserProfileManager(BaseUserManager):
+
+    def create_user(self, username, password=None):
+        if not username:
+            raise ValueError('User must provide username')
+
+        user = self.model(username=username)
+
+        user.set_password(password)
+        user.save()
+
+        return user
+
+    def create_superuser(self, username, password):
+        """create and save a new superuser with given details"""
+        user = self.create_user(username=username, password=password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+
+        return user
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(unique=True, max_length=30)
+
+    f_name = models.CharField(max_length=50, null=True, blank=True)
+
+    l_name = models.CharField(max_length=50, null=True, blank=True)
+
+    mobile = models.CharField(max_length=11, null=True, blank=True)
+
+    email = models.EmailField(null=True, blank=True)
+
+    gender = models.IntegerField(default=0)
+
+    cv = models.FileField(null=True, blank=True)
+
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = []
